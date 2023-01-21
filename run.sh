19a57c73f8e8c312cafd007bfbe8e68ab8f9ee4d
Thomas Krause <thomas.krause@fernuni-hagen.de>
2023-01-21 17:54:07 +0100

    Shell script to run/stop all docker compose scripts

---
+++ run.sh
@@ -0,0 +1,8 @@
+#docker network create distributed_system_network
+docker compose -f airflow/docker-compose.yml up -d
+docker compose -f frontend/docker-compose.yml up -d
+docker compose -f kafka/docker-compose.yml up -d
+docker compose -f microservices/BLAST/docker-compose.yml up -d
+docker compose -f microservices/GCCONTENT/docker-compose.yml up -d
+docker compose -f microservices/REPORT/docker-compose.yml up -d
+docker compose -f microservices/STORAGE/docker-compose.yml up -d
---
+++ stop.sh
@@ -0,0 +1,8 @@
+#docker network create distributed_system_network
+docker compose -f airflow/docker-compose.yml down
+docker compose -f frontend/docker-compose.yml down
+docker compose -f kafka/docker-compose.yml down
+docker compose -f microservices/BLAST/docker-compose.yml down
+docker compose -f microservices/GCCONTENT/docker-compose.yml down
+docker compose -f microservices/REPORT/docker-compose.yml down
+docker compose -f microservices/STORAGE/docker-compose.yml down
